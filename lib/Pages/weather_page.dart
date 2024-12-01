f42cad0dd8978b9e79e268aed6e1fa7e643c9af4
agney <agneysuresh@gmail.com>
2024-12-02 00:12:47 +0530

    minor changes

--- lib/Pages/weather_page.dart
+++ lib/Pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kumori/services/weather_service.dart';
+
+import '../models/weather_model.dart';
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
 
@@ -7,8 +10,45 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+
+  // api key
+  final _weatherService = WeatherService('a11455e875014a24be30b493e1590789');
+  Weather? _weather;
+  // fetch weather
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+  // get weather
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    catch(e){
+      print(e);
+    }
+  }
+  // animations
+  // innit state
+  @override
+  void innitState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Loading city..."),
+            Text('${_weather?.tempreture.round()}°C')
+          ],
+        ),
+      )
+    );
   }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,10 +1,39 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:kumori/models/weather_model.dart';
 import 'package:http/http.dart'as http;
 class WeatherService {
-  static const BASE_URL = 'https://openweathermap.org/';
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apikey;
   WeatherService(this.apikey);
   Future<Weather> getWeather(String cityName) async{
-    final response = await http.get
+    final response = await http.get(Uri.parse('$BASE_URL?q=$cityName&appid=$apikey&units=metric'));
+
+    if (response.statusCode==200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    }
+    else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async{
+    // Get permission from user
+    LocationPermission  permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+    }
+    // Fetch permission part
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+    // Convert location into a list of placemark objects
+    List<Placemark> placemarks = 
+    await placemarkFromCoordinates(position.latitude, position.longitude);
+    // Extract the city name from placemark
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
   }
 }
